d75dbbca92b5844e21f3bc75b7603733601e4fbd
Morten Kolstad <morten.kolstad@bekk.no>
2023-10-02 23:08:13 +0200

    add tests

--- tests/SimplifyTest.elm
+++ tests/SimplifyTest.elm
@@ -482,7 +482,7 @@ a =
     case List.foldl f x << Set.toList of
         ( _, Node [] _ ) ->
             []
-    
+
         ( _, Node _ ({ foldl } :: _) ) ->
             []
 """
@@ -499,7 +499,7 @@ a =
     case foldl f x of
         ( _, Node [] _ ) ->
             []
-    
+
         ( _, Node _ ({ foldl } :: _) ) ->
             []
 """
@@ -540,7 +540,7 @@ a =
     let
         doIt ( _, Node _ ({ foldl } :: _) ) =
             ()
-        
+
         doItBetter x =
             List.foldl f x << Set.toList
     in
@@ -559,7 +559,7 @@ a =
     let
         doIt ( _, Node _ ({ foldl } :: _) ) =
             ()
-        
+
         doItBetter x =
             foldl f x
     in
@@ -16959,6 +16959,8 @@ import Array
 a = Array.get n array
 b = Array.get 0 array
 c = Array.get n (Array.fromList [ 1 ])
+d = Array.get n (Array.repeat m ele)
+e = Array.get 0 (Array.repeat m ele)
 """
                     |> Review.Test.run ruleWithDefaults
                     |> Review.Test.expectNoErrors
@@ -17124,7 +17126,43 @@ import Array
 a = Nothing
 """
                         ]
-        ]
+    , test "should replace Array.get 2 (Array.repeat 5 a) by Just True" <|
+                \() ->
+                """module A exposing (..)
+import Array
+a = Array.get 2 (Array.repeat 5 a)
+"""
+                        |> Review.Test.run ruleWithDefaults
+                        |> Review.Test.expectErrors
+                            [ Review.Test.error
+                                { message = "Array.get with an index out of bounds of the given array will always return Nothing"
+                                , details = [ "You can replace this call by Nothing." ]
+                                , under = "Array.get"
+                                }
+                                |> Review.Test.whenFixed """module A exposing (..)
+import Array
+a = Just a
+"""]    , test "should replace Array.get 10 (Array.repeat 5 a) by Nothing" <|
+                    \() ->
+                    """module A exposing (..)
+import Array
+a = Array.get 10 (Array.repeat 5 a)
+"""
+                            |> Review.Test.run ruleWithDefaults
+                            |> Review.Test.expectErrors
+                                [ Review.Test.error
+                                    { message = "Array.get with an index out of bounds of the given array will always return Nothing"
+                                    , details = [ "You can replace this call by Nothing." ]
+                                    , under = "Array.get"
+                                    }
+                                    |> Review.Test.whenFixed """module A exposing (..)
+import Array
+a = Nothing
+"""
+                                                ]
+                                ]
+
+
 
 
 arraySetTests : Test
